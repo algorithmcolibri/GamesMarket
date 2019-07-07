0a233a0a51daea0e584a65db14f96e155213ba64
Саша Семенюк <sasha.anru@gmail.com>
2019-07-07 14:12:05 +0300

    add view

--- GamesMarket/GamesMarket/Controllers/HomeController.cs
+++ GamesMarket/GamesMarket/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace GamesMarket.Controllers
             return View();
         }
 
+        public ActionResult Games()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
