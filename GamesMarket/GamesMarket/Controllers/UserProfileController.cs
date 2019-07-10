5a298e7646b340b26f57cc0fb6056461f8e7942c
Espiritus8 <name>
2019-07-10 23:25:46 +0300

    add register style

---
+++ GamesMarket/GamesMarket/Controllers/UserProfileController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GamesMarket.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        // GET: UserProfile
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+        public ActionResult Settings()
+        {
+            return View();
+        }
+
+        public ActionResult Wallets()
+        {
+            return View();
+        }
+    }
+}
